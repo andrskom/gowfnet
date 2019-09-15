7efcd940d770c2027d99ecadbb72a3350172d22b
Andrei Skomorokhov <andrskom@gmail.com>
2019-09-15 15:38:27 +0300

    * Add registry. * Replace listener. * Add makefile.

--- net.go
+++ net.go
@@ -7,47 +7,6 @@ type Listener interface {
 	AfterPlaced(state *State, placeID string, subject interface{})
 }
 
-// AutomaticListenerMiddleware is a listener for automatic make transition.
-type AutomaticListenerMiddleware struct {
-	errorLogger func(msg string)
-	net         *Net
-	listener    Listener
-}
-
-// NewAutomaticListenerMiddleware init middleware.
-func NewAutomaticListenerMiddleware(
-	errorLogger func(msg string),
-	net *Net,
-	listener Listener,
-) *AutomaticListenerMiddleware {
-	return &AutomaticListenerMiddleware{
-		errorLogger: errorLogger,
-		net:         net,
-		listener:    listener,
-	}
-}
-
-func (l *AutomaticListenerMiddleware) AfterPlaced(state *State, placeID string, subject interface{}) {
-	if l.listener != nil {
-		l.listener.AfterPlaced(state, placeID, subject)
-	}
-
-	place, err := l.net.GetPlace(placeID)
-	if err != nil {
-		state.SetError(err)
-		return
-	}
-	for _, transition := range place.GetToTransitions() {
-		if transition.isAutomatic {
-			if err := l.net.Transit(state, transition.id, subject); err != nil {
-				if !ErrorIs(ErrCodeStateHasNotTokenInPlace, err) {
-					state.SetError(err)
-				}
-			}
-		}
-	}
-}
-
 // Net is built net.
 type Net struct {
 	startPlace  *Place
@@ -121,6 +80,17 @@ func (n *Net) process(state *State, subject interface{}, fromPlaces []string, to
 	if err := state.MoveTokensFromPlacesToPlaces(fromPlaces, toPlaces); err != nil {
 		return err
 	}
+
+	if len(toPlaces) == 1 {
+		place, err := n.GetPlace(toPlaces[0])
+		if err != nil {
+			return err
+		}
+		if place.isFinished {
+			state.isFinished = true
+		}
+	}
+
 	for _, place := range toPlaces {
 		n.listener.AfterPlaced(state, place, subject)
 	}
