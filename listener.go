d3d01a3d69432495f9dbf0f8e0f1bab1c86ddf3f
Andrei Skomorokhov <andrskom@gmail.com>
2019-09-16 18:21:29 +0300

    Add net to listener.

--- listener.go
+++ listener.go
@@ -2,35 +2,32 @@ package gowfnet
 
 // AutomaticListenerMiddleware is a listener for automatic make transition.
 type AutomaticListenerMiddleware struct {
-	net      *Net
 	listener Listener
 }
 
 // NewAutomaticListenerMiddleware init middleware.
 func NewAutomaticListenerMiddleware(
-	net *Net,
 	listener Listener,
 ) *AutomaticListenerMiddleware {
 	return &AutomaticListenerMiddleware{
-		net:      net,
 		listener: listener,
 	}
 }
 
 // AfterPlaced is implementation of event listener interface.
-func (l *AutomaticListenerMiddleware) AfterPlaced(state *State, placeID string, subject interface{}) {
+func (l *AutomaticListenerMiddleware) AfterPlaced(net *Net, state *State, placeID string, subject interface{}) {
 	if l.listener != nil {
-		l.listener.AfterPlaced(state, placeID, subject)
+		l.listener.AfterPlaced(net, state, placeID, subject)
 	}
 
-	place, err := l.net.GetPlace(placeID)
+	place, err := net.GetPlace(placeID)
 	if err != nil {
 		state.SetError(err)
 		return
 	}
 	for _, transition := range place.GetToTransitions() {
 		if transition.isAutomatic {
-			if err := l.net.Transit(state, transition.id, subject); err != nil {
+			if err := net.Transit(state, transition.id, subject); err != nil {
 				if !ErrorIs(ErrCodeStateHasNotTokenInPlace, err) {
 					state.SetError(err)
 				}
