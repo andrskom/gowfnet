29551b6ef5b92d9d4027d53e6b302a6872f176bf
Andrei Skomorokhov <andrskom@gmail.com>
2020-04-17 16:24:38 +0300

    GH-12 Add start/finish place in places.

---
+++ cfg/validator/finish_place_in_places.go
@@ -0,0 +1,20 @@
+package validator
+
+import "github.com/andrskom/gowfnet/cfg"
+
+type FinishPlaceInPlaces struct {
+}
+
+func NewFinishPlaceInPlaces() *FinishPlaceInPlaces {
+	return &FinishPlaceInPlaces{}
+}
+
+func (s *FinishPlaceInPlaces) Validate(c cfg.Interface) error {
+	for _, place := range c.GetPlaces() {
+		if place.GetID() == c.GetFinish().GetID() {
+			return nil
+		}
+	}
+
+	return NewError().Addf("finish place is not found in places")
+}
---
+++ cfg/validator/start_place_in_places.go
@@ -0,0 +1,20 @@
+package validator
+
+import "github.com/andrskom/gowfnet/cfg"
+
+type StartPlaceInPlaces struct {
+}
+
+func NewStartPlaceInPlaces() *StartPlaceInPlaces {
+	return &StartPlaceInPlaces{}
+}
+
+func (s *StartPlaceInPlaces) Validate(c cfg.Interface) error {
+	for _, place := range c.GetPlaces() {
+		if place.GetID() == c.GetStart().GetID() {
+			return nil
+		}
+	}
+
+	return NewError().Addf("start place is not found in places")
+}
