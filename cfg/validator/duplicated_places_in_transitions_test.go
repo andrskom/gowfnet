e8b7d517e206fea2a79a39e3b3c32d557b6500e9
Andrei Skomorokhov <andrskom@gmail.com>
2020-04-22 22:48:00 +0300

    GH-18 Add duplicated places in transitions validator.

---
+++ cfg/validator/duplicated_places_in_transitions_test.go
@@ -0,0 +1,64 @@
+package validator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/andrskom/gowfnet/cfg"
+)
+
+func TestNewDuplicatedPlacesInTransitions(t *testing.T) {
+	assert.Equal(t, &DuplicatedPlacesInTransitions{}, NewDuplicatedPlacesInTransitions())
+}
+
+func TestDuplicatedPlacesInTransitions_Validate_DuplicatedInFrom_ExpectedErr(t *testing.T) {
+	minCfg := cfg.Minimal{
+		Transitions: map[string]cfg.MinimalTransition{
+			"a": {
+				From: []cfg.StringID{"b", "b", "c"},
+			},
+		},
+	}
+
+	v := NewDuplicatedPlacesInTransitions()
+
+	assert.Equal(
+		t,
+		NewError().Addf("place with id 'b' is duplicated in transition with id 'a' in section from"),
+		v.Validate(minCfg),
+	)
+}
+
+func TestDuplicatedPlacesInTransitions_Validate_DuplicatedInTo_ExpectedErr(t *testing.T) {
+	minCfg := cfg.Minimal{
+		Transitions: map[string]cfg.MinimalTransition{
+			"a": {
+				To: []cfg.StringID{"b", "b", "c"},
+			},
+		},
+	}
+
+	v := NewDuplicatedPlacesInTransitions()
+
+	assert.Equal(
+		t,
+		NewError().Addf("place with id 'b' is duplicated in transition with id 'a' in section to"),
+		v.Validate(minCfg),
+	)
+}
+
+func TestDuplicatedPlacesInTransitions_Validate_CorrectCfg_NoErr(t *testing.T) {
+	minCfg := cfg.Minimal{
+		Transitions: map[string]cfg.MinimalTransition{
+			"a": {
+				To:   []cfg.StringID{"b"},
+				From: []cfg.StringID{"c"},
+			},
+		},
+	}
+
+	v := NewDuplicatedPlacesInTransitions()
+
+	assert.NoError(t,v.Validate(minCfg))
+}
